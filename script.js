0840902ab0403310a99fc8fbc23565490f694c34
Vitor <vitorhenrique_rgt@outlook.com.br>
2023-05-31 18:58:49 -0300

    change background based on media queries

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
-  console.log(img)
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute("alt", "Imagem de Vitor com um fundo escuro")
